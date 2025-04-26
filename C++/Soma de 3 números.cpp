6bd32c8dafe264f759870e66d7663f47e5fa031b
Luan <castro.luanjfmg@gmail.com>
2025-04-26 17:12:38 -0300

    Soma de 3 números informados pelo usuário
    Signed-off-by: Luan <castro.luanjfmg@gmail.com>

---
+++ C++/Soma de 3 números.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <locale.h> //permite o uso de acentos 
+
+using namespace std;
+
+int main(){
+ 	
+	setlocale(LC_ALL, "Portuguese");
+	
+	float numero1, numero2, numero3, soma;
+	
+	cout << "Digite um numero: \n";
+	cin >> numero1;
+	
+	cout << "Digite um numero: \n";
+	cin >> numero2;
+	
+	cout << "Digite um numero: \n";
+	cin >> numero3;
+	
+	soma = numero1 + numero2 + numero3;
+	
+	cout << "A soma dos números informados é: " << soma;
+	
+	
+	system("Pause");
+	return 0;
+}
