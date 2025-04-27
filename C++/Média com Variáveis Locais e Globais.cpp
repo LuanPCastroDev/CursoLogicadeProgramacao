fd875451f1472ff27f428eddf7ffe60892579794
Luan <castro.luanjfmg@gmail.com>
2025-04-27 17:01:40 -0300

    Calculando Média de 4 notas(Variáveis Globais e Locais)
    Signed-off-by: Luan <castro.luanjfmg@gmail.com>

---
+++ C++/Média com Variáveis Locais e Globais.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <locale.h> //permite o uso de acentos 
+
+using namespace std;
+
+
+double nota1VG = 7;
+double nota2VG = 6.9;  
+int main(){
+ 	
+	setlocale(LC_ALL, "Portuguese");
+	
+	double nota3VL, nota4VL, MediaNotas;
+	
+	cout << "Digite o valor da 3º nota: \n";
+	cin >> nota3VL;
+	
+	cout << "Digite o valor da 4º nota: \n";
+	cin >> nota4VL;
+	
+	MediaNotas = (nota1VG + nota2VG +  nota3VL + nota4VL) / 4; 
+	
+	cout << "Média: " <<  MediaNotas << "\n";
+	system("Pause");
+	return 0;
+}
