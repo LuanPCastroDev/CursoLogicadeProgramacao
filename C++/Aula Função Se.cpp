333d9a7f520cfeae96a42a6cef1af7fec513f5dd
Luan <castro.luanjfmg@gmail.com>
2025-05-07 20:08:14 -0300

    Continuação aula função se
    Signed-off-by: Luan <castro.luanjfmg@gmail.com>

--- C++/Aula Função Se.cpp
+++ C++/Aula Função Se.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <locale.h> //permite o uso de acentos 
+#include <locale.h> 
 
 using namespace std;
 
@@ -7,16 +7,30 @@ int main(){
 	
 	setlocale(LC_ALL, "Portuguese");
 	
-	double numero;
+	double numero1,  numero2;
 	
 	cout  << "Informe um número: \n";
-	cin >> numero;
+	cin >> numero1;
 	
-	if(numero >= 5)	{
+	if(numero1 >= 5)	{
 		cout << "O número é maior ou igual a 5.\n";
 	} else {
 		cout << "O número é menor que 5. \n";
 	}
+	
+	cout  << "Informe o 2º número: \n";
+	cin >> numero2;
+	
+	if(numero1 > numero2){
+		cout << "O numero " << numero1 << " é maior que " << numero2 << "\n";
+	} if(numero1 == numero2) {
+		cout << "Os dois números são iguais.";
+
+	} else {
+		cout << "O número " << numero2 << " é maior que " << numero1 << "\n";
+	}
+	
+	
 	
 	
 	
